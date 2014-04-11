88490520416c1c46ab49c5bc09d64c24c51af803
kunal1 <kpatel2222@gmail.com>
2014-04-11 13:55:29 -0400

    Feature three :krunal

---
+++ BRDHC/Feedback/feedback.aspx.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Feedback_feedback : System.Web.UI.Page
+{
+    clsCommon objSendMail = new clsCommon();
+    feedback obj = new feedback();
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+
+        if (!Page.IsPostBack)
+        {
+            _subRebind();
+        }
+    }
+
+
+    protected void insertFeedback(object sender, RepeaterCommandEventArgs e)
+    {
+        TextBox fname = (TextBox)e.Item.FindControl("txt_fname");
+        TextBox lname = (TextBox)e.Item.FindControl("txt_lname");
+        TextBox email = (TextBox)e.Item.FindControl("txt_email");
+        TextBox phone = (TextBox)e.Item.FindControl("txt_phone");
+        TextBox city = (TextBox)e.Item.FindControl("txt_city");
+        TextBox state = (TextBox)e.Item.FindControl("txt_state");
+        TextBox feedback = (TextBox)e.Item.FindControl("txt_feedback");
+        RadioButtonList rblpatient = (RadioButtonList)e.Item.FindControl("r_isPatient");
+        RadioButtonList rblgender = (RadioButtonList)e.Item.FindControl("r_gender");
+        Label message = (Label)e.Item.FindControl("lbl_message");
+
+
+
+        switch (e.CommandName)
+        {
+            case "Insert":
+                _strMessage(obj.commitInsert(fname.Text, lname.Text, rblpatient.SelectedItem.Text, rblgender.SelectedItem.Text, city.Text, state.Text, phone.Text, email.Text, feedback.Text), "Thank you for the feedback");
+                objSendMail.sendEMail(email.Text, "<div><br />" + "<br />Thank you for yor Feedback! <br/>"
+                   + "' <br />We will reply as soon as possible", "(Blind River District Health Centre)", true);
+                _subRebind();
+                break;
+            case "Cancel":
+                _subRebind();
+                break;
+        }
+    }
+
+    private void _subRebind()
+    {
+        rpt_feedback.DataSource = obj.getFeedbacks();
+        rpt_feedback.DataBind();
+    }
+
+    private void _strMessage(bool flag, string str)
+    {
+        if (flag)
+        {
+            Response.Redirect("~/Feedback/feedbackThanku.aspx");
+            lbl_message.Text = str;
+        }
+        else
+            lbl_message.Text = "sorry, unable to submit your feedback";
+
+    }
+}
