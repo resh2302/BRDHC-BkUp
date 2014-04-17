d00821fd0358a729476b81e9078b212189f43724
Jagsir Singh <jagsirsingh@hotmail.com>
2014-04-16 23:38:12 -0400

    Jagsir

--- BRDHC/Admin/healthAlerts.aspx.cs
+++ BRDHC/Admin/healthAlerts.aspx.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿// AUTHOR :  JAGSIR SINGH
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,6 +23,7 @@ public partial class Admin_healthAlerts : System.Web.UI.Page
             Label lblDashboard = Master.dashboardHeading;
             lblDashboard.Text = "ADMIN DASHBOARD : HEALTH ALERTS";
             pnlDetails.Visible = false;
+            // bind the alerts to the list show on the page
             load_Records();
         }
 
@@ -31,6 +33,7 @@ public partial class Admin_healthAlerts : System.Web.UI.Page
     {
         string appName = WebConfigurationManager.AppSettings["appName"].ToString();
         List<sp_SearchHealthAlertsByNameResult> alerts = objHAlerts.getHealthAlerts(appName, txtSearchName.Text);
+        // check if there are records then bind otherwise show message
         if (alerts.Count > 0)
         {
             lblErr.Visible = false;
@@ -46,6 +49,7 @@ public partial class Admin_healthAlerts : System.Web.UI.Page
 
     protected void subToggleForm(object sender, EventArgs e)
     {
+        // toggle the form and list views
         lblErr.Visible = false;
         if (pnlDetails.Visible)
         {
@@ -59,12 +63,15 @@ public partial class Admin_healthAlerts : System.Web.UI.Page
             pnlDetails.Visible = true;
             pnlTable.Visible = false;
             btnSU.Text = "Save";
+            // clear all the control on the form 
             clearControls();
         }
     }
 
     protected void clearControls()
     {
+        // reset ids and clear all textboxes
+        // rebind the list 
         _healthAlertId = Guid.Empty;
         txtATitle.Text = string.Empty;
         txtDesc.Text = string.Empty;
@@ -74,14 +81,14 @@ public partial class Admin_healthAlerts : System.Web.UI.Page
 
     protected void subSaveUpdate(object sender, EventArgs e)
     {
-        //if (Page.IsValid)
-        //{
         MembershipUser user = Membership.GetUser();
         lblErr.Visible = true;
         try
         {
             string title = txtATitle.Text;
             string description = txtDesc.Text;
+            // check if _healthAlertId exists then its update otherwise it is new record
+            // save into database and show records
             if (_healthAlertId == null || _healthAlertId == Guid.Empty)
             {
                 _healthAlertId = Guid.NewGuid();
@@ -97,13 +104,15 @@ public partial class Admin_healthAlerts : System.Web.UI.Page
         }
         catch (Exception ex)
         {
+            // save the exception message in the database and show the message
+            clsCommon.saveError(ex);
             lblErr.Text = ex.Message.ToString();
         }
-        //}
     }
 
     protected void subPublish(object sender, EventArgs e)
     {
+        // this will do the save or update but will also publish the alert. Then the alert will be published on the hoem page
         if (Page.IsValid)
         {
             MembershipUser user = Membership.GetUser();
@@ -113,6 +122,7 @@ public partial class Admin_healthAlerts : System.Web.UI.Page
                 string title = txtATitle.Text;
                 string description = txtDesc.Text;
                 objHAlerts.resetForPublish();
+                // check for save and update
                 if (_healthAlertId == null || _healthAlertId == Guid.Empty)
                 {
                     _healthAlertId = Guid.NewGuid();
@@ -126,6 +136,9 @@ public partial class Admin_healthAlerts : System.Web.UI.Page
                 clsCommon objCom = new clsCommon();
                 string appName = WebConfigurationManager.AppSettings["appName"].ToString();
 
+                // now if publish is successfull
+                // then get all the emails of users from brdhc_AlertSubscribers
+                // send each an email containing the alert as body part./
                 List<sp_getAlertSubscribersResult> objEmails = objCom.sp_getAlertSubscribers(appName);
 
                 foreach (sp_getAlertSubscribersResult record in objEmails)
@@ -175,6 +188,10 @@ public partial class Admin_healthAlerts : System.Web.UI.Page
     }
     protected void lstRecords_ItemDataBound(object sender, DataListItemEventArgs e)
     {
+        // alerts may have large data for description
+        // but show only 200 characters in the list 
+        // but on read only click, show the full description
+        // also change the color according to publish status
         Label lblPublish = (Label)(e.Item.FindControl("lblPublish"));
         if (lblPublish.Text == "Published")
         {
@@ -192,6 +209,7 @@ public partial class Admin_healthAlerts : System.Web.UI.Page
     }
     protected void lstRecords_ItemCommand(object sender, DataListCommandEventArgs e)
     {
+        // perform different functions for edit and delte commands from list
         lblErr.Visible = false;
         _healthAlertId = Guid.Parse(((HiddenField)e.Item.FindControl("hdfHealthAlertId")).Value);
         if (e.CommandName == "editAlert")
